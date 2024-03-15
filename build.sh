ad47b41741a280858930594f95dc6d72ae4d9f03
Archibald Picq <archibald.picq@gmail.com>
2024-03-15 14:55:29 +0100

    show version

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-VERSION=1.4.0
 PACKAGE=ovpn-admin
+
+APP_VERSION=$(grep '"version":' frontend/package.json)
+APP_VERSION=${APP_VERSION##*\": \"}
+APP_VERSION=${APP_VERSION%%\"*}
+
+case "$APP_VERSION" in
+  *.*.*) ;;
+  *) echo "Bad package.json version '$APP_VERSION'"; exit 1;;
+esac
+
 
 PATH=$PATH:~/go/bin
 
@@ -40,17 +49,20 @@ PACKAGE_BUILD_SCRIPT=build
 if [ $SKIP_FRONT = 0 ]; then
   rsync --progress --times --recursive --delete-after ~/bus-ui/src/content/app/openvpn/ ./frontend/src/content/app/openvpn/
   rsync --progress --times --recursive --delete-after ~/bus-ui/src/content/app/shared/services/ble/ ./frontend/src/content/app/shared/services/ble/
+  export APP_VERSION
   cd frontend && npm install && npm run $PACKAGE_BUILD_SCRIPT && cd .. || (echo "Build front failed"; exit 1)
 fi
 
 if [ $SKIP_BACK = 0 ]; then
-  CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -a -tags netgo -ldflags "-X main.version=$VERSION -linkmode external -extldflags -static -s -w"
+  echo "Compile Go backend"
+  CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -a -tags netgo -ldflags "-X main.version=$APP_VERSION -linkmode external -extldflags -static -s -w"
 fi
 
 if [ $SKIP_DEB = 0 ]; then
-  BUILD=$(../update-apt.sh get-build ${PACKAGE}_${VERSION})
+  echo "Publish to apt repository"
+  BUILD=$(../update-apt.sh get-build ${PACKAGE}_${APP_VERSION})
   BUILD=$((BUILD + 1))
-  PACKAGE_NAME=${PACKAGE}_${VERSION}-${BUILD}_amd64
+  PACKAGE_NAME=${PACKAGE}_${APP_VERSION}-${BUILD}_amd64
   BUILD_DIR="deb-build"
   rm -rf $BUILD_DIR
 
@@ -68,7 +80,7 @@ if [ $SKIP_DEB = 0 ]; then
   ## Create control files
   mkdir -p $BUILD_DIR/$PACKAGE_NAME/DEBIAN/
 echo "Package: ${PACKAGE}
-Version: ${VERSION}-${BUILD}
+Version: ${APP_VERSION}-${BUILD}
 Maintainer: Archibald Picq <archibald.picq@gmail.com>
 Depends: ${PACKAGE_DEPENDENCIES}
 Architecture: amd64
--- frontend/src/content/app/layouts/main/main.component.ts
+++ frontend/src/content/app/layouts/main/main.component.ts
@@ -1,12 +1,15 @@
 import {Component} from "@angular/core";
 import {Router} from "@angular/router";
 import {Title} from "@angular/platform-browser";
+import {VERSION} from "../../app.constants"
 
 @Component({
     selector: 'ovpn',
-    templateUrl: './main.component.html'
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.scss'],
 })
 export class MainComponent {
+  public appVersion = VERSION;
     constructor(
         private readonly router: Router,
         private readonly titleService: Title,
