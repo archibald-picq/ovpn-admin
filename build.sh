267f270a429e177d9fd8852fd285e3e34bd6c3bc
Archibald Picq <archibald.picq@gmail.com>
2024-09-25 20:56:56 +0200

    improve build script, decouple .deb build to another task

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-set -e
-
-PACKAGE=ovpn-admin
+set -eo pipefail
 
 APP_VERSION=$(grep '"version":' frontend/package.json)
 APP_VERSION=${APP_VERSION##*\": \"}
@@ -9,18 +7,14 @@ APP_VERSION=${APP_VERSION%%\"*}
 
 case "$APP_VERSION" in
   *.*.*) ;;
-  *) echo "Bad package.json version '$APP_VERSION'"; exit 1;;
+  *) echo "Bad version in package.json '$APP_VERSION'"; exit 1;;
 esac
 
-
 PATH=$PATH:~/go/bin
-
 SKIP_FRONT=0
 SKIP_BACK=0
-SKIP_DEB=0
 BUILD_DEV=0
-
-PACKAGE_DEPENDENCIES="libc6, openvpn, easy-rsa"
+ARCH=amd64
 
 while [ $# -ge 1 ]; do
   case $1 in
@@ -33,8 +27,9 @@ while [ $# -ge 1 ]; do
     --skip-back)
       SKIP_BACK=1
       ;;
-    --skip-deb)
-      SKIP_DEB=1
+    --arch)
+      shift
+      ARCH=$1
       ;;
     *)
       echo "Unsupported option: $1"
@@ -54,50 +49,34 @@ if [ $SKIP_FRONT = 0 ]; then
 fi
 
 if [ $SKIP_BACK = 0 ]; then
-  echo "Compile Go backend"
-  CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -a -tags netgo -ldflags "-X main.version=$APP_VERSION -linkmode external -extldflags -static -s -w"
-fi
+  echo "Compile Go backend for arch '$ARCH' at version '$APP_VERSION'"
+#  CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -a -tags netgo -ldflags "-X main.version=$APP_VERSION -linkmode external -extldflags -static -s -w"
 
-if [ $SKIP_DEB = 0 ]; then
-  echo "Publish to apt repository"
-  BUILD=$(../update-apt.sh get-build ${PACKAGE}_${APP_VERSION})
-  BUILD=$((BUILD + 1))
-  PACKAGE_NAME=${PACKAGE}_${APP_VERSION}-${BUILD}_amd64
-  BUILD_DIR="deb-build"
-  rm -rf $BUILD_DIR
+  # apt install gcc-arm-linux-gnueabi
+  # apt install gcc-arm-linux-gnueabihf
+  # apt install aarch64-linux-gnu-gcc
+  rm -f ./rpiadm
+  case $ARCH in
+    arm)
+      CC=arm-linux-gnueabi-gcc CGO_ENABLED=1 GOOS=linux GOARCH=arm GOARM=7 go build -a -tags netgo -ldflags "-X main.version=$APP_VERSION -linkmode external -extldflags -static -s -w"
+      ;;
+    armhf)
+      CC=arm-linux-gnueabi-gcc CGO_ENABLED=1 GOOS=linux GOARCH=arm GOARM=6 go build -a -tags netgo -ldflags "-X main.version=$APP_VERSION -linkmode external -extldflags -static -s -w"
+      ;;
+    arm64)
+      CC=arm-linux-gnueabi-gcc CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build -a -tags netgo -ldflags "-X main.version=$APP_VERSION -linkmode external -extldflags -static -s -w"
+      ;;
+    aarch64)
+      CC=arm-linux-gnueabi-gcc CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build -a -tags netgo -ldflags "-X main.version=$APP_VERSION -linkmode external -extldflags -static -s -w"
+      ;;
+    *)
+      CGO_ENABLED=1 GOOS=linux GOARCH=${ARCH} go build -a -tags netgo -ldflags "-X main.version=$APP_VERSION -linkmode external -extldflags -static -s -w"
+      ;;
+  esac
 
-  ## create package dir
-  mkdir -p $BUILD_DIR/$PACKAGE_NAME
-
-  ## Add service binary file
-  mkdir -p $BUILD_DIR/$PACKAGE_NAME/usr/sbin/
-  cp ./rpiadm $BUILD_DIR/$PACKAGE_NAME/usr/sbin/ovpn-admin
-
-  ## Add service startup file
-  mkdir -p $BUILD_DIR/$PACKAGE_NAME/lib/systemd/system/
-  cp ./setup/ovpn-admin.service $BUILD_DIR/$PACKAGE_NAME/lib/systemd/system/
-
-  ## Create control files
-  mkdir -p $BUILD_DIR/$PACKAGE_NAME/DEBIAN/
-echo "Package: ${PACKAGE}
-Version: ${APP_VERSION}-${BUILD}
-Maintainer: Archibald Picq <archibald.picq@gmail.com>
-Depends: ${PACKAGE_DEPENDENCIES}
-Architecture: amd64
-Homepage: https://picq.fr
-Description: A GUI to manage OpenVPN" \
-> $BUILD_DIR/$PACKAGE_NAME/DEBIAN/control
-
-  cp ./setup/conffiles $BUILD_DIR/$PACKAGE_NAME/DEBIAN/conffiles
-  cp ./setup/preinst.sh $BUILD_DIR/$PACKAGE_NAME/DEBIAN/preinst
-  cp ./setup/postinst.sh $BUILD_DIR/$PACKAGE_NAME/DEBIAN/postinst
-  cp ./setup/prerm.sh $BUILD_DIR/$PACKAGE_NAME/DEBIAN/prerm
-  cp ./setup/postrm.sh $BUILD_DIR/$PACKAGE_NAME/DEBIAN/postrm
-
-  mkdir -p $BUILD_DIR/$PACKAGE_NAME/etc/default/
-  cp ./setup/default.conf $BUILD_DIR/$PACKAGE_NAME/etc/default/ovpn-admin
-
-  dpkg --build $BUILD_DIR/$PACKAGE_NAME && \
-    ../update-apt.sh add-package $BUILD_DIR/$PACKAGE_NAME.deb && \
-    rm -rf $BUILD_DIR
+  if [ ! -f ./rpiadm ]; then
+    echo "Build failed" 1>&2
+    exit 1
+  fi
+  mv ./rpiadm ./rpiadm-$ARCH
 fi
